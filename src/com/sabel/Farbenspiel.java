26c270d255c2c917dc3a3ad83dfad30abf7be666
Leon Arnold <leon.arnold@t-online.de>
2017-09-21 09:24:57 +0200

    Neue Klasse

--- src/com/sabel/Farbenspiel.java
+++ src/com/sabel/Farbenspiel.java
@@ -4,29 +4,37 @@ import javax.swing.*;
 
 public class Farbenspiel extends JFrame {
 
-    private JButton jbtnGruen;
-    private JButton jbtnRot;
-    private JButton jbtnBlau;
-    private JPanel jpPanel;
+    private JButton jbGruen;
+    private JButton jbRot;
+    private JButton jbBlau;
+    private JPanel jPanel;
 
     public Farbenspiel(){
         this.setTitle("Farbenspiel");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(300, 300);
         this.initComponents();
+        this.iniEvent();
         this.setVisible(true);
     }
 
+    private void iniEvent() {
+        MeinActionListener mal = new MeinActionListener(jPanel);
+        jbBlau.addActionListener(mal);
+        jbGruen.addActionListener(mal);
+        jbRot.addActionListener(mal);
+    }
+
     public void initComponents(){
-            jbtnGruen = new JButton("Grün");
-            jbtnRot = new JButton("Rot");
-            jbtnBlau = new JButton("Blau");
-            jpPanel = new JPanel();
-            jpPanel.add(jbtnGruen);
-            jpPanel.add(jbtnRot);
-            jpPanel.add(jbtnBlau);
-            this.add(jpPanel);
-        }
+        jbGruen = new JButton("Grün");
+        jbRot = new JButton("Rot");
+        jbBlau = new JButton("Blau");
+        jPanel = new JPanel();
+        jPanel.add(jbGruen);
+        jPanel.add(jbRot);
+        jPanel.add(jbBlau);
+        this.add(jPanel);
+    }
 
 
 }
